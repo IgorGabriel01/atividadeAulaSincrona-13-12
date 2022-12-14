9cbe2ae61ac569811f8867251654350dad0c29c4
Igor <igorgabsprofissional@gmail.com>
2022-12-13 21:06:26 -0300

    Atividade 13/12

---
+++ script.js
@@ -0,0 +1,42 @@
+console.log ("Eita to funcionando")
+
+const post01 = document.getElementById("post01");
+console.log(post01);
+
+const post02 = document.getElementById("post02");
+console.log(post02);
+
+const formulario = document.getElementById("formulario");
+console.log(formulario);
+
+const sectionPosts = document.getElementById("posts");
+console.log(sectionPosts);
+
+const datasPosts = document.getElementsByClassName("post-data");
+console.log(datasPosts);
+
+const textoPosts = document.getElementsByClassName("post-texto");
+console.log(textoPosts);
+
+const listasRedes = document.getElementsByClassName("lista_redes");
+console.log(listasRedes);
+
+const elementos_nav = document.getElementsByClassName("elementos_nav");
+console.log(elementos_nav);
+
+
+
+const linkPrimeiroPost = document.querySelector(".post p a")
+console.log(linkPrimeiroPost)
+
+const negritoSegundoPost = document.querySelector("#post02 .post-texto strong")
+console.log(negritoSegundoPost)
+
+const nomeForm = document.querySelector("form div label")
+console.log(nomeForm)
+
+const linksFooter = document.querySelectorAll(".lista_redes li a")
+console.log(linksFooter)
+
+const apenasLinkFooter = document.querySelectorAll("nav ul li a")
+console.log(apenasLinkFooter)
